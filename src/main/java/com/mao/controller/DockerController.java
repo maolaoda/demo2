f9411336ce79c37b18851a0adbc8ad4b203bb88a
maolaoda <1164504411@qq.com>
2019-12-13 17:00:07 +0800

    Initial commit

---
+++ src/main/java/com/mao/controller/DockerController.java
@@ -0,0 +1,20 @@
+package com.mao.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerController {
+
+        @RequestMapping("/")
+        public String index(){
+            return "嘀嘀嘀";
+        }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerController.class, args);
+    }
+}
